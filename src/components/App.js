c336c330d65bde47e8be1b974f722e329b4124fe
+ <+>
2021-08-23 16:01:16 -0400

    COMPLETE

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,34 @@
 import video from "../data/video.js";
+import React, { useState } from "react"
+import VideoContent from "./VideoContent.js";
+import CommentList from "./CommentList.js";
+import Title from "./Title"
 
 function App() {
-  console.log("Here's your data:", video);
+  const [upvotes, setUpvotes] = useState(video.upvotes)
+  const [downvotes, setDownvotes] = useState(video.downvotes)
+  const [hide, setHide] = useState(true)
+
+  function handleUpvote() {
+    setUpvotes(upvotes + 1)
+  }
+
+  function handleDownvote() {
+    setDownvotes(downvotes + 1)
+  }
+
+  function handleHide() {
+    setHide((hide) => !hide)
+  }
+
+  const displayComments = hide ? <CommentList video={video} hide={hide} /> : null
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <VideoContent />
+      <Title video={video} upvotes={upvotes} downvotes={downvotes} handleUpvote={handleUpvote} handleDownvote={handleDownvote} handleHide={handleHide} />
+      <br /><hr />
+      {displayComments}
     </div>
   );
 }
